b9948ea8cca1badd2947a0f8b60b42a844592e75
Cathy Thuliez <popschool@debian>
2017-07-09 23:50:05 +0200

    Update code

--- intro-js/js/suite-fibonacci.js
+++ intro-js/js/suite-fibonacci.js
@@ -1,15 +1,15 @@
 function fibonacci()
 {
-  var nb_1 = 0, nb_2 = 1;
+  var number_1 = 0, number_2 = 1;
 
   for(var i = 0; i < 2500; i++)
   {
-    var result = nb_1 + nb_2;
+    var result = number_1 + number_2;
 
     console.log(result);
 
-    nb_1 = nb_2;
-    nb_2 = result;
+    number_1 = number_2;
+    number_2 = result;
   }
 }
 
