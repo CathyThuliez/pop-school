469d32800108a3f44d67ac32960a6682651466c3
CathyThuliez <popschool@debian>
2017-07-10 12:00:36 +0200

    Add file

--- exo-js/js/exo-13-js.js
+++ exo-js/js/exo-13-js.js
@@ -1,8 +1,10 @@
 var allGreen = function() {
-  var div = document.getElementsByTagName('div');
-  for (i=0; i<div.length; i++) {
-    div[i].style = 'background-color: green;';
-  }
+  // var div = document.getElementsByTagName('div');
+  // for (i=0; i<div.length; i++) {
+  //   div[i].style = 'background-color: green;';
+  // }
+  r2g();
+  b2g();
 }
 var r2g = function() {
   var div = document.getElementsByClassName('red');
